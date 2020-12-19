58faab470685bae8a0e1e918259ee24cf087171a
Timothy Sturm <timothysturm6@gmail.com>
2020-12-19 01:53:55 -0600

    Proof of Concept - quality of life

--- src/Ford.Pass.Connect/PassiveLocker/Program.cs
+++ src/Ford.Pass.Connect/PassiveLocker/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using System;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace PassiveLocker
 {
@@ -13,26 +15,41 @@ namespace PassiveLocker
         static void Main(string[] args)
         {
             var serviceProvider = ConfigureServices(args);
-
             var client = serviceProvider.GetService<Client>();
+            Console.WriteLine("Connecting to FordPass...");
             client.Auth().Wait();
             var command = FordCommand.Lock;
             var response = client.IssueCommand(command).Result;
 
+            int i = 1;
             CommandStatus result;
             do
             {
+                Console.Write("\r                          ");
+                Console.Write("\rIssuing Command");
+                for(var c = i; c > 1; c--)
+                {
+                    Console.Write(".");
+                }
                 result = client.CommandStatus(command, response).Result;
+                i = (i > 3) ? 1 : i + 1;
+                Thread.Sleep(500);
             } while (result.Status == 552);
-
+            
+            var defaultForeground = Console.ForegroundColor;
             if (result.Status == 200)
             {
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Success");
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Failure");
             }
+            Console.ForegroundColor = defaultForeground;
+
+            Console.WriteLine("Done");
         }
 
         private static IServiceProvider ConfigureServices(string[] args)
@@ -40,6 +57,7 @@ namespace PassiveLocker
             IConfigurationBuilder configBuilder = new ConfigurationBuilder()
                 .SetBasePath(Path.Combine(AppContext.BaseDirectory))
                 .AddJsonFile("appsettings.json", true)
+                .AddJsonFile("appsettings.dev.json", true)
                 .AddEnvironmentVariables()
                 .AddCommandLine(args);
 
